db4d7430b7a1e998e584db11a836ef011a79301f
Vijayant Pawar <vijayant.pawar@bennett.edu.in>
2026-05-23 14:22:22 +0530

    Add Courses to the Homepage

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ with col1:
     st.image('mogli.jpg')
 with col2:
     st.header('CampusX is an online Platform')
+
+st.header('Courses')
+st.subheader('DSMP')
+st.subheader('DAMP')
+st.subheader('DEMP')
